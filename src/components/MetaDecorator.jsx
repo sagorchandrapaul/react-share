365a02e3da0e9b80a40345cbffa89d7c756b48ec
sagor_paul <sagor.paul@shadhinlab.com>
2022-05-10 12:40:49 +0600

    seven commit

--- src/components/MetaDecorator.jsx
+++ src/components/MetaDecorator.jsx
@@ -10,7 +10,7 @@ const MetaDecorator = ({title, description, imageUrl, imageAlt}) => (
         <meta property="og:title" content={title}/>
         <meta name="description" content={description}/>
         <meta property="og:description" content={description}/>
-        <meta property="og:image" content={window.location.origin + imageUrl}/>
+        <meta property="og:image" content={imageUrl}/>
         <meta property="og:url" content={window.location.pathname + window.location.search}/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:image:alt" content={imageAlt}/>
